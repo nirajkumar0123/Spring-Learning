70042b73a49766e749a95b98fdf16a6a47b0aacc
Niraj Kumar <nirajgupta7481@gmail.com>
2025-02-21 20:18:24 +0530

    [Niraj] Add . display hello from bridgelabz

---
+++ uc1/src/test/java/com/tit/springlearning/uc1/Uc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tit.springlearning.uc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
